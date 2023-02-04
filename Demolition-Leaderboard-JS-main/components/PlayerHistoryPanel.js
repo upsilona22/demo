077836ba228804bcb62c5006c21a96d40e649224
upsilona22 <123645115+upsilona22@users.noreply.github.com>
2023-02-04 14:51:00 +0100

    Delete Demolition-Leaderboard-JS-main directory

--- Demolition-Leaderboard-JS-main/components/PlayerHistoryPanel.js
+++
@@ -1,8 +0,0 @@
-import PlayerHistoryTable from '@components/PlayerHistoryTable'
-export default function PlayerHistoryPanel({ rowData }) {
-    return (
-        <div className="playerHistoryPanel">
-          {PlayerHistoryTable({ rowData })}
-        </div>
-    )
-}
