b863a4eb20ccc546c94d2fd291a779f74866b43c
upsilona22 <123645115+upsilona22@users.noreply.github.com>
2023-02-04 14:37:50 +0100

    Add files via upload

---
+++ Demolition-Leaderboard-JS-main/netlify/functions/downloadData.js
@@ -0,0 +1,30 @@
+const fetch = require('isomorphic-fetch');
+const Dropbox = require('dropbox').Dropbox;
+let dbx = new Dropbox({accessToken: process.env.dropToken, fetch: fetch});
+
+exports.handler = async function(event, context) {
+    try {
+        const response = await dbx.filesDownload({path: "/leaderboard.json"});
+
+        if (response.status !== 200) {
+            return {
+                statusCode: response.status,
+                message: "Dropbox error"
+            }
+        }
+
+        const data = JSON.parse(response.result.fileBinary);
+
+        return {
+            statusCode: 200,
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(data)
+        }
+    } catch(err) {
+        console.log(err)
+        return {
+            statusCode: 502,
+            message: "Error connecting to dropbox"
+        }
+    }
+}
